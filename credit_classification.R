726d029a0992c1729533751bd926572d84c4b0b4
Ramon Ferreira Silva <rsilva@ramonsilva.net>
2016-12-06 10:30:23 -0200

    Criando Tabelas de frequência
    Passe 1 - Naive bayes : Criar tabelas de frequência

--- credit_classification.R
+++ credit_classification.R
@@ -1,2 +1,23 @@
 dataset = read.csv("dataset.csv", header = TRUE, sep = ",", quote = "\"",
                    +          dec = ".", fill = TRUE, comment.char = "")
+
+
+classificar(objeto, dataset_treinamento){
+  #Criando as tabelas de frequencia
+  
+  #idade
+  ages = dataset$age
+  ages.freq = table(ages)
+  
+  #ganhos
+  incomes = dataset$income
+  incomes.freq = table(incomes)
+  
+  #estudante ?
+  student = dataset$student
+  student.freq = table(student)
+  
+  #nota de crédito
+  credit_rating <- dataset$credit_rating
+  credit_rating.freq = table(credit_rating)
+}
